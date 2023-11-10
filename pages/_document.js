b30306ec0057f02a84c5cc268e3b5ab27d86f85e
aidan <aidanb363@gmail.com>
2023-11-10 10:16:06 -0500

    url logo

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="icon" href="/images/mhb-removebg-preview.png" type="image/x-icon" />
+          {/* other head elements */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
